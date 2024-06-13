674bc15b79f37b84aae83629591c527312f9b5df
danvirbhargava <danvir.bhargava@students.mq.edu.au>
2024-06-13 17:44:05 +1000

    attempting to figure out how to read div tables

--- scra.py
+++ scra.py
@@ -2,8 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 stats_url = "https://www.uefa.com/european-qualifiers/statistics/teams/"
-headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"}
-data = requests.get(stats_url, headers=headers)
-soup = BeautifulSoup(data.text, 'html.parser')
-standings_table = soup.find('div', class_='ag-center-cols-viewport')
-print(standings_table)
+##headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"}
+##data = requests.get(stats_url, headers=headers)
+##soup = BeautifulSoup(data.content, 'html.parser')
+##standings_table = soup.find_all("div",{"class" : "ag-center-cols-container"})
+##print(standings_table)
+from requests_html import HTMLSession
+
+session = HTMLSession()
+data = session.get(stats_url)
+print(data)
